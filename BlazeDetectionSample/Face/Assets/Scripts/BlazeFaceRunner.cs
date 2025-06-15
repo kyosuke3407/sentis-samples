73e581ac25f385fa89ec892a80a50a5665838b72
kyosuke3407 <110596672+kyosuke3407@users.noreply.github.com>
2025-06-15 13:02:03 +0900

    BBox

--- BlazeDetectionSample/Face/Assets/Scripts/BlazeFaceRunner.cs
+++ BlazeDetectionSample/Face/Assets/Scripts/BlazeFaceRunner.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class BlazeFaceRunner : MonoBehaviour
 {
-    [SerializeField]private ModelAsset BlazeFaceAsset;
+    [SerializeField] private ModelAsset BlazeFaceAsset;
     private Model BlazeFaceModel;
     private Worker worker;
     private Tensor<float> inputTensor, outputTensor;
@@ -20,8 +20,8 @@ public class BlazeFaceRunner : MonoBehaviour
     public float scoreThreshold = 0.5f;
 
     [SerializeField] private Texture2D tex;
-    public RawImage rawImage;
-    private RenderTexture renderTexture;
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private Material material;
 
     public static float[,] LoadAnchors(string csv, int k)
     {
@@ -31,7 +31,7 @@ public class BlazeFaceRunner : MonoBehaviour
         for (int i = 0; i < k; i++)
         {
             var anchorValues = anchorLines[i].Split(',');
-            for (var j = 0; j < 4;  j++)
+            for (var j = 0; j < 4; j++)
             {
                 anchors[i, j] = float.Parse(anchorValues[j], CultureInfo.InvariantCulture);
             }
@@ -43,8 +43,8 @@ public class BlazeFaceRunner : MonoBehaviour
     public static (FunctionalTensor, FunctionalTensor, FunctionalTensor) NWSF(FunctionalTensor rawBoxes, FunctionalTensor rawScores, FunctionalTensor anchors, float iouTreshold, float scoreThreshold)
     {
         //uv
-        var xCenter = rawBoxes[0, .., 0] + anchors[.., 0];
-        var yCenter = rawBoxes[0, .., 1] + anchors[.., 1] * 128;  
+        var xCenter = rawBoxes[0, .., 0] + anchors[.., 0] * 128;
+        var yCenter = rawBoxes[0, .., 1] + anchors[.., 1] * 128;
 
         var widthHalf = 0.5f * rawBoxes[0, .., 2];
         var heightHalf = 0.5f * rawBoxes[0, .., 3];
@@ -65,7 +65,6 @@ public class BlazeFaceRunner : MonoBehaviour
         var selectedScores = Functional.IndexSelect(rawScores, 1, selectedIndies).Unsqueeze(0);
 
         return (selectedIndies, selectedScores, selectedBoxes);
-
     }
 
     void Start()
@@ -76,7 +75,7 @@ public class BlazeFaceRunner : MonoBehaviour
         var graph = new FunctionalGraph();
 
         var input = graph.AddInput(BlazeFaceModel, 0);
-        var outputs = Functional.Forward(BlazeFaceModel,　2 * input - 1);
+        var outputs = Functional.Forward(BlazeFaceModel, 2 * input - 1);
 
         var boxes = outputs[0];
         var scores = outputs[1];
@@ -87,30 +86,7 @@ public class BlazeFaceRunner : MonoBehaviour
         var anchors = Functional.Constant(new TensorShape(k_NumAnchors, 4), ancohorData);
         var idxScoreBox = NWSF(boxes, scores, anchors, iouThreshold, scoreThreshold);
 
-        //input = input[.., 60..128, 60..128, ..];
-
-        //input = Functional.Permute(input, new[] { 0, 3, 1, 2 });
-        //var resized =  Functional.Interpolate(input, 
-        //                                      size: new int[] {192, 192},
-        //                                      mode: "linear");
-
-        var selectedIndies = idxScoreBox.Item1;
-        var selectedAnchor = Functional.IndexSelect(anchors, 0, selectedIndies);
-        selectedAnchor = selectedAnchor * 128.0f;
-        selectedAnchor = selectedAnchor[.., 0..2];
-
-        var boxN = idxScoreBox.Item3;
-
-
-
-        //var left = AnchorX - box[0, 0, 2] * 0.5f;
-        //var right = AnchorX + box[0, 0, 2] * 0.5f;
-        //var top = AnchorY + box[0, 0, 3] * 0.5f;
-        //var bottom = AnchorY - box[0, 0, 3] * 0.5f;
-
         var AttentionMeshModel = graph.Compile(idxScoreBox.Item1, idxScoreBox.Item2, idxScoreBox.Item3);
-        ////var AttentionMeshModel = graph.Compile(resized);
-        //var AttentionMeshModel = graph.Compile(boxN,selectedAnchor);
 
 
         worker = new Worker(AttentionMeshModel, BackendType.GPUCompute);
@@ -124,28 +100,6 @@ public class BlazeFaceRunner : MonoBehaviour
         worker.SetInput(0, inputTensor);
 
         worker.Schedule();
-
-        //var output = worker.PeekOutput(0) as Tensor<float>;
-        //if (output != null)
-        //{
-        //    float[] values = output.DownloadToArray();
-        //    // 先頭10個だけ表示（大量の場合の例）
-        //    for (int i = 0; i < Mathf.Min(10, values.Length); i++)
-        //    {
-        //        Debug.Log($"output[{i}] = {values[i]}");
-        //    }
-        //    Debug.Log($"output length: {values.Length}");
-        //}
-        //else
-        //{
-        //    Debug.LogWarning("output is null or not a Tensor<float>");
-        //}
-
-        //var output = worker.PeekOutput(0) as Tensor<float>;
-        //Debug.Log("Output shape: " + output.shape);
-
-        //renderTexture = TextureConverter.ToTexture(output);
-        //rawImage.texture = renderTexture;
 
         var indexFace = worker.PeekOutput(0) as Tensor<int>;
         var score = worker.PeekOutput(1) as Tensor<float>;
@@ -167,6 +121,18 @@ public class BlazeFaceRunner : MonoBehaviour
         Debug.Log("boxSpaceUnderLeft: " + boxSpace);
         Debug.Log("boxSpaceTopRight: " + boxTopRightSpace);
 
+        var x = boxSpace.x - cpuBox[0, 0, 2] * 0.5f;
+        var y = Mathf.Abs((boxSpace.y - cpuBox[0, 0, 3] * 0.5f) - 128);
+
+        Debug.Log("boxSpaceUnderLeft: " + x + ", " + y + ", " + cpuBox[0, 0, 2] + ", " + cpuBox[0, 0, 3]);
+
+        rawImage.texture = tex;
+        Vector4 box = new Vector4(x, y, cpuBox[0, 0, 2], cpuBox[0, 0, 3]);
+        material.SetVector("_Box", box);
+
+
+        //Debug.Log("anchors: " + 128 * m_Anchors[cpuIndex[0], 0] + ", " + 128 * m_Anchors[cpuIndex[0], 1]);
+        //Debug.Log("boxSpace: " + cpuBox[0, 0, 0] + ", " + cpuBox[0, 0, 1] + ", " + cpuBox[0, 0, 3] + ", " + cpuBox[0, 0, 4]);
     }
 
 
@@ -188,7 +154,7 @@ public class BlazeFaceRunner : MonoBehaviour
         //Debug.Log(cpuScore);
         //Debug.Log(cpuBox);
         //Debug.Log(cpuIndex);
-        
+
 
     }
 }
